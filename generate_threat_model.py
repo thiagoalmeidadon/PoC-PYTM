477caedb3ac8eac65a30c9119259432fa133278b
thiagoalmeidadon <thiagodons@gmail.com>
2025-03-04 16:50:07 -0300

    pytm, semgrep, scrpt, integrando

--- generate_threat_model.py
+++ generate_threat_model.py
@@ -25,9 +25,9 @@ tm = TM("Modelo de Ameaças")
 tm.description = "Modelo gerado a partir dos resultados do Semgrep."
 
 
-usuario = Actor("Usuário")
-servidor = Server("Servidor Web")
-banco_dados = Datastore("Banco de Dados")
+usuario = Actor("usuario")
+servidor = Server("servidor")
+banco_dados = Datastore("banco_dados")
 
 
 Dataflow(usuario, servidor, "Requisição HTTP")
@@ -49,7 +49,7 @@ for idx, resultado in enumerate(semgrep_data.get("results", []), start=1):
     threat_code = next((code for key, code in mapeamento_ameacas.items() if key.lower() in mensagem.lower()), "INP14")
 
     
-    alvo = servidor if "XSS" in mensagem else banco_dados
+    alvo = "servidor" if "XSS" in mensagem else "banco_dados"
 
     
     Threat(
